969355ad8310680563af4fa17670365a1ff27f2b
lasha <lsumbadze@crocobet.ge>
2020-12-18 21:51:21 +0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+export interface ExampObj {
+  name: string;
+  lastname?: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'hello-world1999';
+  title = 'hello, ეს არის angular 1 პროექტი';
+  isDisabled = false;
+  inputValue: string = '';
+
+  logNameClicker() {
+    this.logName({name: 'lasha', lastname: 'sumbadze'})
+  }
+
+  logName(human: ExampObj) {
+    console.log(human);
+    this.inputValue = "teqsturi";
+  }
+
+  inputFunction(event: any) {
+    console.log(event.target.value * 4);
+    this.inputValue = event.target.value;
+  }
 }
